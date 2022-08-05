d43426f8ebeca8445cfb24807859e459c79961f4
gold-dennar <gdennar@icitsolutions.com>
2022-08-05 14:39:01 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// var openNav = document.getElementsById("mySidebar");
-// var openSideNav = document.getElementsByClassName("navContainer");
-// var closeNav = document.getElementsByClassName("navContainer");
+
 
 function openNav(){
     document.getElementById("mySidebar").style.width="250px";
     document.getElementById("mobile-sideNav").style.zIndex="0";
     document.getElementById("main").style.background="linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3))";
+    // document.getElementById("project").style.boxShadow="none" ;
 }
 
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.background="none";
+    document.getElementById("main").style.opacity="1";
 }
