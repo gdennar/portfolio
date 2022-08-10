8b03b37ea05456f0f599b0da9cedb5380a15bdc6
gold-dennar <gdennar@icitsolutions.com>
2022-08-10 17:40:31 +0100

    Adjust screen about page on mobile view

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function openNav(){
     document.getElementById("mySidebar").style.width="250px";
     document.getElementById("mobile-sideNav").style.zIndex="0";
     document.getElementById("main").style.background="linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3))";
-
+    
     var element = document.querySelectorAll(".skill-items");
     for (let i = 0; i < element.length; i++) {
         element[i].classList.add("skill-item");
